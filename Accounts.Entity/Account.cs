6ec4466b67a400c905a865e8d0d5e591037bcb88
sivaselvi106 <selvisanjeev106@gmail.com>
2020-02-19 22:43:39 +0530

    commit

---
+++ Accounts.Entity/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts.Entity
+{
+    public class Account
+    {
+        public string UserName { get; set; }
+        public string MailId { get; set; }
+        public string Password { get; set; }
+        public string Age { get; set; }
+        public string Gender { get; set; }
+        public long MobileNo { get; set; }
+        public string City { get; set; }
+    }
+}
--- MobileShopping/App_Start/RouteConfig.cs
+++ MobileShopping/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace MobileShopping
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Index", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
